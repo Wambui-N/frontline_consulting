8fc5e2fe5b9349c3ed74f7223b8d3902595a916e
Wambui Ndungu <wambuin18@gmail.com>
2024-01-29 14:57:39 +0300

    Refactor Footer component to include navigation links and logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-export default Footer
+  const isCurrentPage = (path: string) => {
+    return pathname === path;
+  };
+  return (
+    <div className="mx-4 sm:mx-[8vw] flex flex-col">
+      <div className="flex justify-center">
+        <div className="flex sm:flex-row flex-col items-center justify-center">
+          <Link
+            className={`mx-[15px] hover:text-orange ${
+              isCurrentPage("/") ? "text-orange" : "text-black"
+            }`}
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={`mx-[15px] hover:text-orange ${
+              isCurrentPage("/About") ? "text-orange" : "text-black"
+            }`}
+            href="/About"
+          >
+            About
+          </Link>
+          <Link
+            className={`mx-[15px] hover:text-orange ${
+              isCurrentPage("/Services") ? "text-orange" : "text-black"
+            }`}
+            href="/Services"
+          >
+            Services
+          </Link>
+          <Link
+            className={`mx-[15px] hover:text-orange ${
+              isCurrentPage("/Portfolio") ? "text-orange" : "text-black"
+            }`}
+            href="/Portfolio"
+          >
+            Portfolio
+          </Link>
+          <Link
+            className={`mx-[15px] hover:text-orange ${
+              isCurrentPage("/Contact") ? "text-orange" : "text-black"
+            }`}
+            href="/Contact"
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
+      <div className="my-[15px] w-full h-1 border-t-[0.2vh] border-black/25"></div>
+      <div className="flex sm:flex-row flex-col justify-between items-center">
+        <p className="mx-[15px]">© 2023 Frontline Consulting. All rights reserved</p>
+        <Image
+          className="inline-block h-[96px] w-auto mt-[10px] max-w-full object-contain"
+          src="/Dark Logo L.png"
+          width={250}
+          height={250}
+          alt="Frontline Consulting Logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
